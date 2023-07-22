cf548374d12d87a54cd29c5b5cda9c75a7710010
veralara.bernhard <veralara.bernhard@uzh.ch>
2023-07-22 23:46:57 +0200

    Add basic locations

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert'; // Import the dart:convert library
+import 'dart:convert';
 import 'dart:math';
-import 'package:intl/intl.dart'; // Import the intl library
+import 'package:intl/intl.dart';
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
+import 'package:geolocator/geolocator.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'SBB Roulette',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromRGBO(198, 0, 24, 1.0)),
+          seedColor: const Color.fromRGBO(198, 0, 24, 1.0),
+        ),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'SBB Roulette'),
@@ -29,7 +30,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title});
 
   final String title;
 
@@ -43,32 +44,22 @@ class _MyHomePageState extends State<MyHomePage> {
   String _destination = '';
   String _station = '';
   String _mode = '';
-  String _delay = '';
+  String _delay = 'null';
   String _modeIdentifier = '';
   String _error = '';
 
   String formatDateTime(String dateTimeString) {
-    // Load the time zone data
     tz.initializeTimeZones();
-
-    // Parse the input date string into a DateTime object
     DateTime dateTime = DateTime.parse(dateTimeString);
-
-    // Get the time zone for GMT+2 (Central European Time, CET)
     tz.Location gmtPlus2 = tz.getLocation('Europe/Paris');
-
-    // Convert the input DateTime to GMT+2
     tz.TZDateTime gmtPlus2DateTime = tz.TZDateTime.from(dateTime, gmtPlus2);
-
-    // Format the time as HH:mm
     String formattedTime = DateFormat('HH:mm').format(gmtPlus2DateTime);
-
     return formattedTime;
   }
 
-  // Define a map that maps each mode to its corresponding icon
   final Map<String, IconData> modeIcons = {
     'S': Icons.train,
+    'SN': Icons.train,
     'IR': Icons.train,
     'IC': Icons.train,
     'ICE': Icons.train,
@@ -80,14 +71,11 @@ class _MyHomePageState extends State<MyHomePage> {
   };
 
   Future<void> _getRandomDeparture() async {
-    // Replace 'YOUR_API_ENDPOINT' with the actual API endpoint URL
-
     const int limit = 10;
     final String api =
         'http://transport.opendata.ch/v1/stationboard?station=$_station&limit=$limit';
     final response = await http.get(Uri.parse(api));
 
-    // generate a random number between 0 and limit
     var random = Random();
     var randomNumber = random.nextInt(limit);
 
@@ -99,8 +87,7 @@ class _MyHomePageState extends State<MyHomePage> {
           _error = 'Bahnhof/Haltestelle nicht gefunden';
         });
       } else {
-        // API request was successful
-
+        _error = '';
         final connection = responseData['stationboard'][randomNumber];
 
         setState(() {
@@ -115,8 +102,6 @@ class _MyHomePageState extends State<MyHomePage> {
         });
       }
     } else {
-      // API request failed
-      // You can handle errors here, e.g., show a snackbar or error message
       print('API request failed with status code: ${response.statusCode}');
     }
   }
@@ -136,7 +121,6 @@ class _MyHomePageState extends State<MyHomePage> {
             MyCustomForm(
               onStationChanged: (station) {
                 setState(() {
-                  print(station);
                   _station = station;
                 });
               },
@@ -148,67 +132,76 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             const SizedBox(height: 30),
             if (_error != '') ...[
-              Text(_error,
-                  style: const TextStyle(
-                    fontSize: 18.0, // Set the font size to 24
-                    fontWeight: FontWeight.bold, // Set the font weight to bold
-                    color: Colors.red,
-                  ))
-            ],
-            if (_departureTime != '') ...[
-              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                // conditional icon depending on mode
-                Icon(
-                  modeIcons[_mode],
-                  color: Theme.of(context).colorScheme.primary,
+              Text(
+                _error,
+                style: const TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red,
                 ),
-                const SizedBox(width: 5),
-                Text('$_modeIdentifier',
-                    style: const TextStyle(
-                      fontSize: 18.0, // Set the font size to 24
-                      fontWeight:
-                          FontWeight.bold, // Set the font weight to bold
-                    )),
-                const SizedBox(width: 10),
-                Text('Richtung $_destination',
-                    style: const TextStyle(
-                      fontSize: 18.0, // Set the font size to 24
-                      fontWeight:
-                          FontWeight.bold, // Set the font weight to bold
-                    )),
-              ])
+              ),
             ],
             if (_departureTime != '') ...[
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text('Abfahrt um $_departureTime',
+                  Icon(
+                    modeIcons[_mode],
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                  const SizedBox(width: 5),
+                  Text(
+                    '$_modeIdentifier',
+                    style: const TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Text(
+                    'Richtung $_destination',
+                    style: const TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+            if (_departureTime != '') ...[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Abfahrt um $_departureTime',
+                    style: const TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  if (_delay != 'null') ...[
+                    Text(
+                      '+$_delay',
                       style: const TextStyle(
-                        fontSize: 18.0, // Set the font size to 24
-                        fontWeight:
-                            FontWeight.bold, // Set the font weight to bold
-                      )),
-                  if (_delay != '0') ...[
-                    Text('+$_delay',
-                        style: const TextStyle(
-                          fontSize: 12.0, // Set the font size to 24
-                          fontWeight:
-                              FontWeight.bold, // Set the font weight to bold
-                          color: Colors.red,
-                        )),
+                        fontSize: 12.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.red,
+                      ),
+                    ),
                   ],
-                  SizedBox(width: 5),
+                  const SizedBox(width: 5),
                   if (_track != '') ...[
-                    Text('auf Gleis $_track',
-                        style: const TextStyle(
-                          fontSize: 18.0, // Set the font size to 24
-                          fontWeight:
-                              FontWeight.bold, // Set the font weight to bold
-                        )),
+                    Text(
+                      'auf Gleis $_track',
+                      style: const TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ],
                 ],
-              )
-            ]
+              ),
+            ],
           ],
         ),
       ),
@@ -228,6 +221,71 @@ class MyCustomForm extends StatefulWidget {
 
 class _MyCustomFormState extends State<MyCustomForm> {
   final TextEditingController _stationController = TextEditingController();
+  List<dynamic> _possibleStations = [];
+
+  Future<String?> getCurrentLocationString() async {
+    bool isLocationServiceEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if (!isLocationServiceEnabled) {
+      return null;
+    }
+
+    LocationPermission permission = await Geolocator.checkPermission();
+
+    if (permission == LocationPermission.deniedForever) {
+      return null;
+    }
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+
+      if (permission != LocationPermission.whileInUse &&
+          permission != LocationPermission.always) {
+        return null;
+      }
+    }
+
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.best,
+    );
+
+    String locationString = '${position.latitude},${position.longitude}';
+    return locationString;
+  }
+
+  final Map<String, IconData> modeIcons = {
+    'sl-icon-type-strain': Icons.train,
+    'sl-icon-type-bus': Icons.directions_bus,
+    'sl-icon-type-tram': Icons.tram,
+    'sl-icon-type-ship': Icons.directions_boat,
+    // Add more mode-icon pairs as needed
+  };
+
+  Future<void> _getClosestStations() async {
+    String? locationString = await getCurrentLocationString();
+
+    if (locationString == null) {
+      return;
+    } else {
+      final String api =
+          'https://timetable.search.ch/api/completion.json?latlon=$locationString';
+      final response = await http.get(Uri.parse(api));
+
+      if (response.statusCode == 200) {
+        var responseData = jsonDecode(response.body);
+        responseData
+            .removeWhere((item) => item["iconclass"] == "sl-icon-type-adr");
+        var possibleStations =
+            responseData.sublist(0, min<int>(5, responseData.length));
+
+        setState(() {
+          _possibleStations = possibleStations;
+        });
+      } else {
+        print('API request failed with status code: ${response.statusCode}');
+      }
+    }
+  }
 
   @override
   void dispose() {
@@ -259,10 +317,7 @@ class _MyCustomFormState extends State<MyCustomForm> {
               ),
               IconButton(
                 icon: Icon(Icons.location_on),
-                onPressed: () {
-                  // Add the action you want to perform when the button is pressed
-                  print('IconButton pressed!');
-                },
+                onPressed: _getClosestStations,
                 color: Theme.of(context).colorScheme.primary,
               ),
             ],
